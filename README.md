49063115d46b8ff801e6478ffa58a4d1734f3e21
Ruta Haile <haile.ruta@gmail.com>
2024-08-15 16:14:32 -0400

    added starter files

---
+++ README.md
@@ -0,0 +1,6 @@
+### Capstone Project
+
+#### Team Members
+1. Tianna Brown
+2. Oumou Diallo
+3. Ruta Haile
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+// const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => console.log("Server Started"));
