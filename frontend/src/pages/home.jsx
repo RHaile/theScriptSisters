57fa3b9816feedc0534702e3feb0d183a97b8a23
Oumou <OumouCDiallo>
2024-08-27 16:02:48 -0400

    Added CSS and updated JSX for Home page styling

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import '../../css/home.css';
 
 function Home() {
-  return <h2>Home Page</h2>;
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Seed Circle</h1>
+        <p className="py-6">
+        From Seed to Plate
+        </p>
+        <button className="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+  );
 }
 
 export default Home;
