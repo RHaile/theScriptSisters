08f44c7ec4152311065bd5778a28bca558d41f0d
1Tianna <tiannacbrown@yahoo.com>
2024-08-29 20:51:33 -0400

    linked sign in page to log in page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function Login() {
-    return (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-  
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
-            <div>
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+        <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
@@ -56,18 +58,18 @@ function Login() {
                 Sign in
               </button>
             </div>
-          </form>
-  
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-primary hover:text-white">
-              Join Seed Circle Today!
-            </a>
-          </p>
-        </div>
+
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{' '}
+          <Link to="/signup" className="font-semibold leading-6 text-primary hover:text-white">
+            Join Seed Circle Today!
+          </Link>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Login;
-
