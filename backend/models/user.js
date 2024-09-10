211264f3d8fdddc1f87f55734916e50e39bdcd5a
dfols <dylanjacobfoley@gmail.com>
2024-09-10 16:07:58 -0400

    add email field to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,10 +4,16 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
 });
 
